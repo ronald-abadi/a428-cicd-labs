a5d79ec327664a7334e351411ce20ea5b5e8acd9
ronald-abadi <136207363+ronald-abadi@users.noreply.github.com>
2023-07-03 08:56:38 +0700

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+echo '------------------------------------------------------------'
+echo 'DBS Foundation Coding Camp 2023 with Dicoding'
+echo 'Belajar Implementasi CI/CD'
+echo 'Deploy Stage, running kill.sh'
+echo 'submission-cicd-pipeline-rabadi'
+echo 'Run by Ronald Mulyono Abadi'
+echo '------------------------------------------------------------'
+
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
